ad98df44642ca87bc807b3784f0ca0b83c2360a7
ThunderBall64 <33467576+ThunderBall64@users.noreply.github.com>
2019-05-06 16:43:04 -0400

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is <b>Default Index</b>";
+        //}
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Helllo " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
